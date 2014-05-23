0c2b7d18fbed6399b74247a9ce6e95d852b26746
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-22 22:25:15 -0700

    update 4-22-14

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
 
+// Change color of selected option //
 $('.choice').on('click', function() {
+	console.log('changecolor');
 	$('.choice').removeClass('selectedAnswer');
 	$(this).addClass('selectedAnswer');
 });
 
+// Clear Questions //
+function q1clear() {
+	$('.quiz-questions').hide();
+	$('.picture').hide();
+	$('#q1').hide();
+}
+ 
+// Check Answer //
+function checkAnswer(question,answer) {
+var rightAnswer = 0;
+if(question == 'q1' && answer =='5'){
+	$('.correct').show();
+	rightAnswer++;
+	}
+else if(question == 'q1' && answer !== '5') {
+	$('.incorrect').show();
+	}
+}
+
+// Continue //
+$('.ok').on('click', function() {
+	$('.correct').hide();
+	$('.incorrect').hide();
+	});
 });

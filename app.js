7b0da24c8f5571c9d6994d653c57e13d2db508e6
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-23 19:22:30 -0700

    Update 4-23-14

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
+var answers = {
+	q1:5,
+	q2: "Michael Jordan",
+	q3: 'Thunders',
+	q4: 'Damian Lillard'
+};
+
 $(document).ready(function() {
 
 // Change color of selected option //
 $('.choice').on('click', function() {
-	console.log('changecolor');
+	var answer = parseInt($(this).find('.text').text(), 10);
+	var question = $(this).parent().prop('id');
+	
 	$('.choice').removeClass('selectedAnswer');
 	$(this).addClass('selectedAnswer');
+	
+	checkAnswer(question, answer);
+});
+
+// Restart Quiz
+	$('.restart').on('click', function(){
+		var current = parseInt($('.quiz-questions').data('current'), 10);
+		advanceQuestion(current, 1);
+	});
+
+// Continue //
+	$('.ok').on('click', function() {
+		var current = parseInt($('.quiz-questions').data('current'), 10);
+		var next = current + 1;
+		
+		$('.correct').hide();
+		$('.incorrect').hide();
+		
+		advanceQuestion(current, next);
+		$('.quiz-questions').data('current', next);
+	});
 });
 
 // Clear Questions //
-function q1clear() {
-	$('.quiz-questions').hide();
-	$('.picture').hide();
-	$('#q1').hide();
+function advanceQuestion(current, next) {
+		$('.question' + current).hide();
+		$('.answers' + current).hide();
+		$('.image' + current).hide();
+		
+		$('.question' + next).show();
+		$('.answers' + next).show();
+		$('.image' + next).show();
 }
  
 // Check Answer //
 function checkAnswer(question,answer) {
-var rightAnswer = 0;
-if(question == 'q1' && answer =='5'){
-	$('.correct').show();
-	rightAnswer++;
+	var rightAnswer = 0;
+	if(answers[question] === answer){
+		$('.answer-status').addClass('correct').removeClass('incorrect').show();
+		rightAnswer++;
 	}
-else if(question == 'q1' && answer !== '5') {
-	$('.incorrect').show();
+	else{
+		$('.incorrect').show();
 	}
 }
-
-// Continue //
-$('.ok').on('click', function() {
-	$('.correct').hide();
-	$('.incorrect').hide();
-	});
-});
